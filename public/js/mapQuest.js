84a67a689276ce29957cc19bd87e2ac448adf034
joshpaz0 <46551500+joshpaz0@users.noreply.github.com>
2019-02-28 09:18:14 -0800

    Update mapQuest.js

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'g7PpsKo65TmPrG3B1ZcST5dzlGH7oUxS';
+	
+	var map = / .mapquest.map('map' {
+	center: [32.88, -117.236],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+});
+
+L.marker([32.88, -117.236]).addTo(map);
 }
